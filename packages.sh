d81e7de22edd8eb0f2538686edf6acc3503052ad
CaptnJayce <cjest.socials@gmail.com>
2024-09-18 23:48:13 +0100

    new packages

--- packages.sh
+++ packages.sh
@@ -3,12 +3,15 @@
 # I run a minimal EnOS installation with hyprland on top, it is PERSONALISED for me. I do not recommend running this script unless you are me
 # I am not liable for any problems or conflicts this may cause in your system 
 
-
 # pacman packages
-pacman -S networkmanager wofi stow kitty flatpak curl yay hyprland hyprpaper neovim discord qtcreator git gcc raylib yazi fastfetch lua nodejs npm sddm ttf-jetbrains-mono-nerd ttf-hack-nerd 
+pacman -S vulkan-radeon lib32-vulkan-radeon networkmanager wofi stow kitty flatpak curl yay hyprland hyprpaper neovim steam discord git gcc raylib yazi fastfetch lua nodejs npm sddm ttf-jetbrains-mono-nerd ttf-hack-nerd 
+
+# unintall
+pacman -R amdvlk lib32-amdvlk
 
 # packages that can't be installed via pacman 
-yay -S spotify hyprshot waybar-git --noconfirm
+# if it fails just copy paste command manually
+yay -S jetbrains-toolbox spotify hyprshot waybar-git --noconfirm
 
 # flatpaks
 flatpak install flathub com.ultimaker.cura
